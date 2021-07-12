47046019b4a8ae9b078f1afc214547eb17921f09
seangomez <seangomez44@gmail.com>
2021-07-12 13:15:56 -0400

    added new elements to homepage

--- js/main.js
+++ js/main.js
@@ -1,45 +1,21 @@
 $(document).ready(function(){
 
-        var count = 0;
-        $("#cog, #cogOne, #cogTwo").click(function(){
-                        
-			count += 1;
-			
-                        if(count== 1) {
-                                $("#cog, #cogOne, #cogTwo").css("animation-play-state", "running");
-                                }
-                        else if(count == 3){
-                                $("#cog, #cogOne, #cogTwo").css("animation-play-state", "running");
-                        } 
-			else if(count == 5){
-                                $("#cog, #cogOne, #cogTwo").css("animation-play-state", "running");
-                        } 
-			else if(count == 7){
-                                $("#cog, #cogOne, #cogTwo").css("animation-play-state", "running");
-                        } 
-			else if(count == 9){
-                                $("#cog, #cogOne, #cogTwo").css("animation-play-state", "running");
-                        } 
-
-                        else{ $("#cog, #cogOne, #cogTwo").css("animation-play-state", "paused")};
-                        });
-                                                      
-                        
-
-});
+	$('#secondParagraph').hide();
 
 
-$("#cog, #cogOne, #cogTwo").hover(function(){
 
-	$("div.wrapper").innerHTML="<h1>hi</h1>";
+	$('#Paragraph').click(function(){
+		$('#Paragraph').fadeOut(800);
+		$('#secondParagraph').fadeIn(1000);
+	});
+	
+	$('#secondParagraph').click(function(){
+		$('#secondParagraph').fadeOut(800);
+		$('#Paragraph').fadeIn(1000);
+	});
 
 
 });
-
-
-
-
-
 
 
 
